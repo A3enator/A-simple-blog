f3b5553a58f39690567ceeac86bdd2a1c7fa603d
3enaTor <siyavashamir686@gmail.com>
2024-09-09 04:21:33 -0700

    add test cod in project
    add test cod in project and write TODO

--- posts/tests.py
+++ posts/tests.py
@@ -1,5 +1,30 @@
+from django.urls import reverse
 from django.test import TestCase
+from .models import Post
 
-    # TODO : add test in code to naxt update ...
+# TODO : add test in code to naxt update ...
+
 
 # Create your tests here.
+class PostModelTest(TestCase):
+    def setUp(self):
+        Post.objects.create(text="Just a test text!")
+
+    def test_text_contant(self):
+        Post.objects.create(text="just a test text!")
+        post = Post.objects.get(text="just a test text!")
+        expected_object_name = f"{post.text}"
+        self.assertEqual(expected_object_name, "just a test text!")
+
+    def test_view_url_exists_at_proper_location(self):
+        resp = self.client.get("/")
+        self.assertEqual(resp.status_code, 200)
+
+    def test_view_url_by_name(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+
+    def test_veiw_uses_correct_template(self):
+        resp = self.client.get(reverse("home"))
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, "home.html")
